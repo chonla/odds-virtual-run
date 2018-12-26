49f343051a297234decd9133bd7fdfc363344d37
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-12-26 23:41:37 +0700

    Add some test

--- src/app/components/vr-detail/vr-detail.component.spec.ts
+++ src/app/components/vr-detail/vr-detail.component.spec.ts
@@ -1,25 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { VrDetailComponent } from './vr-detail.component';
+import { VrService } from 'src/app/services/vr.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { ActivatedRoute, Router, ActivatedRouteSnapshot, ParamMap } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { Vr } from 'src/app/models/vr';
 
 describe('VrDetailComponent', () => {
   let component: VrDetailComponent;
-  let fixture: ComponentFixture<VrDetailComponent>;
+  const MockVrService: jest.Mock<VrService> = jest.fn<VrService>(() => ({
+    get: jest.fn().mockReturnValue((new Subject<Vr>()).asObservable())
+  }));
+  const MockAuthService: jest.Mock<AuthService> = jest.fn<AuthService>(() => ({
+    whoami: jest.fn().mockReturnValue('1111')
+  }));
+  const MockActivatedRoute: jest.Mock<ActivatedRoute> = jest.fn<ActivatedRoute>(() => ({
+    snapshot: {
+      paramMap: {
+        get: jest.fn().mockReturnValue('1234abcd')
+      }
+    }
+  }));
+  const MockRouter: jest.Mock<Router> = jest.fn<Router>(() => ({
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ VrDetailComponent ]
-    })
-    .compileComponents();
   }));
 
+  let mockVrService: VrService;
+  let mockAuthService: AuthService;
+  let mockActivatedRoute: ActivatedRoute;
+  let mockFormBuilder: FormBuilder;
+  let mockRouter: Router;
+
   beforeEach(() => {
-    fixture = TestBed.createComponent(VrDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockVrService = new MockVrService();
+    mockAuthService = new MockAuthService();
+    mockActivatedRoute = new MockActivatedRoute();
+    mockFormBuilder = new FormBuilder();
+    mockRouter = new MockRouter();
+
+    component = new VrDetailComponent(mockVrService, mockAuthService, mockActivatedRoute, mockFormBuilder, mockRouter);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load vr from param ID in url', () => {
+    component.ngOnInit();
+
+    expect(mockVrService.get).toHaveBeenCalledWith('1234abcd');
   });
 });
