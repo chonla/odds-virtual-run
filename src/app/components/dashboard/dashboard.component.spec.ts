ed91a313669a33fe541dd594067e76ae9ad60302
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-12-26 15:36:18 +0700

    Add more tests

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,12 +1,16 @@
 import { DashboardComponent } from './dashboard.component';
 import { VrService } from 'src/app/services/vr.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { Subject } from 'rxjs';
+import { Athlete } from 'src/app/models/athlete';
+import { Version } from '@angular/core';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
 
   const MockVrService: jest.Mock<VrService> = jest.fn<VrService​​>(() => ({
-
+    me: jest.fn().mockReturnValue((new Subject<Athlete​​>()).asObservable()),
+    version: jest.fn().mockReturnValue((new Subject<Version>()).asObservable())
   }));
   const MockAuthService: jest.Mock<AuthService> = jest.fn<AuthService​​>(() => ({
     signOut: jest.fn()
@@ -22,6 +26,13 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get my stats and load app version when initialized', () => {
+    component.ngOnInit();
+
+    expect(mockVrService.me).toHaveBeenCalled();
+    expect(mockVrService.version).toHaveBeenCalled();
   });
 
   it('should call signout', () => {
